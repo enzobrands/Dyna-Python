c51e7e53a35d9b62c3cef9f385e41ea9426a9186
Maarten Heremans <maarten.heremans@dynactionize.com>
2017-06-07 20:29:04 +0200

    Added basic CSV loader

--- dyna/dynizer/loaders/__init__.py
+++ dyna/dynizer/loaders/__init__.py
@@ -1,3 +1,6 @@
 from .xml_loader import XMLAbstractElement, XMLFixedElement, XMLVariableElement, XMLExtractionElement, XMLStringCombinationElement
 from .xml_loader import XMLLoopVariable, XMLMapping, XMLLoader
 
+from .csv_loader import CSVAbstractElement, CSVFixedElement, CSVRowElement, CSVStringCombinationElement
+from .csv_loader import CSVMapping, CSVLoader
+
